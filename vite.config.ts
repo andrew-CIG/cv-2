85bb43c93ef6cc0788a76dcae648fffb82a22c44
andrew-CIG <araymer@figr.com>
2025-09-17 00:20:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import { fileURLToPath } from 'node:url'
+import { URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
