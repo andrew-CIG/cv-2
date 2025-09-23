8d3f76fec7bae6f909787d975d917a14774542e9
Andrew Raymer <araymer@figr.com>
2025-09-23 01:30:52 -0300

    Update CVOther.vue

---
+++ src/components/CVOther.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Other from "@/assets/snippets/other.md";
+import { marked } from "marked";
+</script>
+
+<template>
+  <div class="other">
+    <h2>Hobbies & Interests</h2>
+    <p v-html="marked.parse(Other)"></p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
