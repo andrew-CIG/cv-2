f7b76d265600d5a0d8228914b803b5e97df60755
Andrew Raymer <araymer@figr.com>
2025-09-29 19:30:30 -0300

    Update CVExperience.vue

--- src/components/CVExperience.vue
+++ src/components/CVExperience.vue
@@ -357,8 +357,8 @@ div.experiences {
     }
 
     div.line {
-      page-break-before: avoid;
-      page-break-after: avoid;
+      /* page-break-before: avoid; */
+      /* page-break-after: avoid; */
       margin-left: auto;
       margin-right: auto;
       background-color: var(--color-position-line);
@@ -367,17 +367,17 @@ div.experiences {
     }
 
     div.experience-desc {
-      page-break-before: avoid;
-      page-break-after: auto;
+      /* page-break-before: avoid; */
+      /* page-break-after: auto; */
 
       div.positions {
-        page-break-before: avoid;
+        /* page-break-before: avoid; */
         display: flex;
         flex-direction: column;
         row-gap: 1.8em;
 
         div.position {
-          page-break-before: auto;
+          /* page-break-before: auto; */
 
           div.title { font-weight: bold; font-size: 1em; }
 
@@ -444,7 +444,7 @@ div.experiences {
     }
   }
 }
-.page-break { display: none; }
+/* .page-break { display: none; } */
 
 @media print {
   .page-break {
