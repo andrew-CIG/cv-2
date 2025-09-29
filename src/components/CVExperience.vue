59e80f713452173f10e191c98cbdaadbf3e99715
Andrew Raymer <araymer@figr.com>
2025-09-29 10:04:44 -0300

    Update CVExperience.vue

--- src/components/CVExperience.vue
+++ src/components/CVExperience.vue
@@ -456,7 +456,7 @@ div.experiences {
 
   div.experiences { 
     /* display: block !important; */
-    break-inside: auto;
+    /* break-inside: auto; */
 
     div.company{
       column-gap: 24px;
